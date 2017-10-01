dfa4046a38a395cbdcc54c713441c890c484e23e
Matt Tilley <mtilley255@gmail.com>
2017-10-01 15:41:49 -0500

    Made modal appear / disappear as appropriate.

--- JS/primary.js
+++ JS/primary.js
@@ -1,7 +1,19 @@
 $(document).ready(function()
 {
 
+	$("#SEC03").hide();
 	
+	$("#TFT01").on("click", function()
+	{
+		$("#SEC02").hide();
+		$("#SEC03").show();
+	});
+	
+	$("#BUT06").on("click", function()
+	{
+		$("#SEC03").hide();
+		$("#SEC02").show();
+	});
 
 	
 	
