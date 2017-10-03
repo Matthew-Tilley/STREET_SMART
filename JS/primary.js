322dae7dafd06d71619f95c864af08447c2e3367
Matt Tilley <mtilley255@gmail.com>
2017-10-03 08:23:56 -0500

    Alpha keyboard wired up & sending values.

--- JS/primary.js
+++ JS/primary.js
@@ -3,18 +3,29 @@ $(document).ready(function()
 
 	$("#SEC03").hide();
 	
-	$("#TFT01").on("click", function()
+	$("#TFT01").on("click", function()			/*ACTIVATES ON SCREEN KEYBOARD*/
 	{
 		$("#SEC02").hide();
 		$("#SEC03").show();
+		
 	});
+		
 	
-	$("#BUT06").on("click", function()
+	$("#BUT06").on("click", function()			/*HIDES ON SCREEN KEYBOARD*/
 	{
 		$("#SEC03").hide();
 		$("#SEC02").show();
 	});
-
+	
+	
+	$(".BUT01").on("click", function()
+	{
+		console.log($(this).val());
+	});
 	
 	
 });
+
+
+
+
